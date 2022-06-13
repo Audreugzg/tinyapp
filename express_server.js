1f3d27309c63911847c40b530cbf7b2c206bad2f
Zuge Gao <audreygaozu@hotmail.com>
2022-06-13 23:16:39 +0000

    add a function that returns a string of 6 random alphanumeric characters

--- express_server.js
+++ express_server.js
@@ -5,6 +5,15 @@ const PORT = 8080; // default port 8080
 //set ejs as the view engine
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  const result = Math.random().toString(36).substring(2,8);
+  return result;
+
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -32,6 +41,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 
 
